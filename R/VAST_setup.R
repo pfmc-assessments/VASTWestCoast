08ed7f169c98c3f2124bbe3e3c27f5655e8a12be
Kelli Johnson <kellifayejohnson@gmail.com>
2018-09-12 03:46:33 -0700

    Calculate standardized depth before mean depth per knot Use JTT's covariate code, the depth term was the exact same as what I was doing but not the depth squared because I was squaring after taking the mean. Also, I added the standardized depth squared where before I was using just depthsquared. Now depth is standardized and then that value is squared and then the mean for each knot is calculated.

--- R/VAST_setup.R
+++ R/VAST_setup.R
@@ -31,10 +31,14 @@ VAST_setup <- function(data, dir, regionacronym, strata = NULL, nknots) {
   # White paper that advocates for standardizing before squaring for a quadratic
   # term to reduce the correlation between the variables.
   unstandarddepth <- Extrapolation_List$Data_Extrap[, "Depth_km"]
+  Extrapolation_List$Data_Extrap$indkm <- Extrapolation_List$Data_Extrap[, "Depth_km"]
+  Extrapolation_List$Data_Extrap$indkms <- Extrapolation_List$Data_Extrap[, "Depth_km2"]
   Extrapolation_List$Data_Extrap[, "Depth_km"] <-
     (Extrapolation_List$Data_Extrap[, "Depth_km"] -
      mean(Extrapolation_List$Data_Extrap[, "Depth_km"])
     ) / sd(Extrapolation_List$Data_Extrap[, "Depth_km"])
+  Extrapolation_List$Data_Extrap[, "Depth_km2"] <-
+    Extrapolation_List$Data_Extrap[, "Depth_km"]^2
   Spatial_List <- suppressMessages(FishStatsUtils::make_spatial_info(
     n_x = nknots,
     Method = "Mesh",
@@ -49,16 +53,36 @@ VAST_setup <- function(data, dir, regionacronym, strata = NULL, nknots) {
   data <- cbind(data, "knot_i" = Spatial_List$knot_i)
 
   # Generate covariate
+  # Yields an array where the mean is taken per knot of the
+  # input data, thus squaring happens before taking the mean.
   Year_Set <- seq(min(data[, "Year"]), max(data[, "Year"]))
-  X_xtp <- get_depth(Extrapolation_List$Data_Extrap,
-    Spatial_List$PolygonList$NN_Extrap$nn.idx,
-    max(which(Year_Set %in% sort(unique(data[, "Year"])))))
+  depthperknot <- suppressMessages(FishStatsUtils::format_covariates(
+    Lat_e = Extrapolation_List$Data_Extrap$Lat,
+    Lon_e = Extrapolation_List$Data_Extrap$Lon,
+    t_e = data$Year[
+      match(Spatial_List$NN_Extrap$nn.idx, data$knot_i)
+      ],
+    Cov_ep = Extrapolation_List$Data_Extrap[, c("Depth_km", "Depth_km2")],
+    Extrapolation_List = Extrapolation_List,
+    Spatial_List = Spatial_List,
+    FUN = mean,
+    Year_Set = Year_Set,
+    na.omit = "time-average"))
+  # Code to square after calculating for a mean per knot
+  # X_xtp <- get_depth(Extrapolation_List$Data_Extrap,
+  #   Spatial_List$PolygonList$NN_Extrap$nn.idx,
+  #   max(which(Year_Set %in% sort(unique(data[, "Year"])))))
+  X_xtp <- depthperknot$Cov_xtp
   # Change depth in Extrapolation_List so that it matches EM
-  Extrapolation_List$Data_Extrap[, "Depth_km"] <- X_xtp[,1,1][Spatial_List$PolygonList$NN_Extrap$nn.idx]
+  Extrapolation_List$Data_Extrap[, "Depth_km"] <-
+    X_xtp[, 1, 1][Spatial_List$PolygonList$NN_Extrap$nn.idx]
+  Extrapolation_List$Data_Extrap[, "Depth_km2"] <-
+    X_xtp[, 1, 2][Spatial_List$PolygonList$NN_Extrap$nn.idx]
   return(list(
     "data" = data,
     "Extrapolation_List" = Extrapolation_List,
     "unstandarddepth" = unstandarddepth,
+    "depthperknot" = depthperknot,
     "Spatial_List" = Spatial_List,
     "X_xtp" = X_xtp))
 }
