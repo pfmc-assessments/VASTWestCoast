265f29cb5a3fd963486a9ae5204453951e51b623
kellijohnson <kellifayejohnson@gmail.com>
2018-10-24 14:44:57 -0700

    Update the test to be true

--- tests/testthat/test-minimalexample.R
+++ tests/testthat/test-minimalexample.R
@@ -4,6 +4,8 @@ wd.old <- getwd()
 temp_path <- file.path(tempdir(), "eg")
 dir.create(temp_path, showWarnings = FALSE)
 setwd(temp_path)
+version <- tail(gsub(".cpp", "", 
+    dir(system.file("executables", package = "VAST"))), 1)
 
 Sim_Settings <- list(
   "Species" = "WCGBTS_Anoplopoma_fimbria",
@@ -11,7 +13,7 @@ Sim_Settings <- list(
   "nknots" = 250,
   "strata" = data.frame("STRATA" = "All_areas"),
   "depth" = c("no", "linear", "squared")[1],
-  "version" = "VAST_v4_0_0",
+  "version" = version,
   "Passcondition" = FALSE)
 
 test_that("Testing database is saved in VAST_condition", {
@@ -26,13 +28,15 @@ test_that("Testing database is saved in VAST_condition", {
   base::load(file = "Save.RData", envir = e1)
   newnames <- ls(envir = e1)
   expect_true(all(newnames %in%
-    c("AIC", "Index", "Obj", "Opt", "ParHat",
-      "Report", "sdreport", "TmbData", "TmbList")),
+    c("AIC", "error", "Index", "Obj", "Opt", "ParHat",
+      "Report", "sdreport", "tables", "TmbData", "TmbList")),
     info = "names in the file saved after conditioning changed")
   expect_equivalent(get("AIC", envir = e1), 56879,
     tolerance = 1, info = "checking the AIC of the sablefish run")
 })
 
 setwd(wd.old)
-# todo: find the dll files and remove them from R before the following
+dyn.unload(getLoadedDLLs()[[
+  grep(version, names(getLoadedDLLs()))]][["path"]])
+
 unlink(temp_path, recursive = TRUE)
