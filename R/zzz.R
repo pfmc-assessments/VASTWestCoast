712a7ea4ad8e49982c42ea460813ade00c897ef3
Kelli Johnson <kelli.johnson@noaa.gov>
2019-02-15 05:43:11 -0800

    Change to onLoad rather than onAttach
    Not sure if I even need INLA in here now b/c I
    made it a dependent package. It doesn't hurt though.
    Also no longer need James-Thorson packages b/c
    I have remotes for them. The issue was with INLA not
    bringing up the window to set a mirror when it was
    being installed through James-Thorson/package, but he
    added similar code to what I have here to fix it for me in
    his development branches.
    The change to onLoad is so that it will be called when someone
    uses a function for the first time rather than just when
    library(VASTWestCoast) gets called. This seemed more ideal
    because I use the function calls all the time for a package
    rather than loading the entire thing.

--- R/zzz.R
+++ R/zzz.R
@@ -1,19 +1,17 @@
 
-.onAttach <- function(libname, pkgname) {
+.onLoad <- function(libname, pkgname) {
   test <- function(pkg) {
     thetest <- tryCatch(find.package(pkg), 
       error = function(e) e)
     ifelse("error" %in% class(thetest), TRUE, FALSE)
   }
-  if (test("devtools")){
-    # packageStartupMessage("Installing package: ")
-    utils::install.packages("devtools", 
-      repos = getOption("repos"), dependencies = TRUE)
+  if (getOption("repos")["CRAN"] == "@CRAN@") {
+    options(repos = c("CRAN" = "http://cran.us.r-project.org"))
   }
-  if (test("nwfscSurvey")){
-   devtools::install_github("nwfsc-assess/nwfscSurvey")
-  }
-  if (test("VAST")){
-   devtools::install_github("James-Thorson/VAST")
+  if (test("INLA")) {
+    install.packages("INLA", 
+      repos = c(getOption("repos"), 
+        INLA = "https://inla.r-inla-download.org/R/stable"),
+      dep = TRUE)
   }
 }
