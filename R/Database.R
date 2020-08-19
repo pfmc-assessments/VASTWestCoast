173f03610d7149e3102ea0f343219311c5df3979
kellijohnson-NOAA <kelli.johnson@noaa.gov>
2020-08-19 14:54:41 -0700

    doc(Database): wrong brackets in docs and not compressed

--- R/Database.R
+++ R/Database.R
@@ -3,7 +3,7 @@
 #' An example data base pull that was used for the 2019 sablefish stock
 #' assessment. This data is used for testing purposes and to demonstrate
 #' examples. The data set was generated using
-#' \code{\link[nwfscSurvey](PullCatch.fn)}.
+#' \code{\link[nwfscSurvey]{PullCatch.fn}()}.
 #'
 #' @name Database
 #' @docType data
