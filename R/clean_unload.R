7d3cbfe40dd69f53f8beffcbb8f7ee846be5e84d
kellijohnson-NOAA <kelli.johnson@nooa.gov>
2020-09-27 08:13:40 -0700

    feat(clean_unload): unload the dll

---
+++ R/clean_unload.R
@@ -0,0 +1,18 @@
+#' Unload VAST dll
+#'
+#' Unload a dll library with the VAST name.
+#'
+#' @param searchfor A file name of the dll to search for.
+#'
+#' @export
+#' @author Kelli Faye Johnson
+#' @return The name(s) of the unloaded libraries are returned.
+#'
+clean_unload <- function(searchfor = "VAST") {
+  dlls <- getLoadedDLLs()
+  filenames <- lapply(dlls, "[[", "path")
+  remove <- gsub("\\.dll", "", grep(searchfor, filenames, value = TRUE))
+  if (length(remove) < 1) return(NULL)
+  ignore <- lapply(remove, function(x) dyn.unload(TMB::dynlib(x)))
+  return(filenames)
+}
