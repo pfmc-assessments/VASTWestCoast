47355a4b5095d8b553e2f22af5ab1d4f45001186
kellijohnson-NOAA <kelli.johnson@nooa.gov>
2020-09-29 21:56:17 -0700

    feat(get_depthbin): plot depth bins

---
+++ R/get_depthbin.R
@@ -0,0 +1,38 @@
+#' Get Depth by Year
+#'
+#' Get depth of positive and empty tows by year plotted in a histogram.
+#'
+#' @param df A data frame from the NWFSC warehouse.
+#' @param vline The x-axis location of the vertical line.
+#' The default is to include a line at 366 m because of the
+#' change in sampled depths in the Triennial Survey. A value of
+#' \code{vline = NULL} will remove the line from the plot.
+#'
+#' @author Kelli Faye Johnson
+#' @return A ggplot object.
+#' @examples
+#' get_depthbin(Database)
+#' \dontrun{
+#' tridata <- nwfscSurvey::PullCatch.fn(SurveyName = "Triennial")
+#' tridata[, "Catch_KG"] <- tridata[, "total_catch_wt_kg"]
+#' for (ii in unique(tri$Common_name)) {
+#'   dd <- subset(tri, Common_name == ii)
+#'   if (nrow(dd) == 0) next
+#'   g <- get_depthbin(dd)
+#'   g <- g + ggtitle(ii)
+#'   ggsave(g, file = paste0(tempdir(), "Tri_", ii, ".png"))
+#' }
+#' }
+#'
+get_depthbin <- function(df, vline = 366) {
+  df[, "year"] <- factor(df[, "Year"])
+  df[, "positive"] <- ifelse(df[, "Catch_KG"] > 0,
+    "positive tow", "empty tow")
+
+  gg <- plot_multi_histogram(df = df, col.feature = "Depth_m", col.label ="year") +
+    ggplot2::facet_grid(positive ~ .) +
+    ggplot2::geom_vline(xintercept = vline, color = "black",
+      linetype = "dashed", size = 1) +
+    ggplot2::xlab("Depth (m)") + ggplot2::theme_bw()
+  return(gg)
+}
---
+++ R/plot_multi_histogram.R
@@ -0,0 +1,29 @@
+#' Plot Multiple Histograms
+#'
+#' Plot multiple histograms for a given column (x axis) by
+#' category (colors). Both the stacked histogram and individual
+#' density lines are included, where the colors coincide and are
+#' labelled in the margin.
+#'
+#' @param df A data frame.
+#' @param col.feature A character value specifying the column of \code{df}
+#' that will be used for the x axis.
+#' @param col.label A character value specifying the column of \code{df}
+#' that will be used for the colors or indivdual lines.
+#'
+#' @author Kelli Faye Johnson
+#' @return A ggplot object.
+#'
+plot_multi_histogram <- function(df, col.feature, col.label) {
+  plt <- ggplot2::ggplot(df,
+    ggplot2::aes(
+      x = .data[[col.feature]],
+      fill = .data[[col.label]])) +
+  ggplot2::geom_histogram(alpha = 0.5, position = "identity",
+    ggplot2::aes(y = ..density..), color = "black") +
+  ggplot2::geom_density(alpha = 0.5) +
+  labs(x = col.feature, y = "Density") + 
+  ggplot2::guides(fill = ggplot2::guide_legend(title = col.label))
+  return(plt)
+
+}
