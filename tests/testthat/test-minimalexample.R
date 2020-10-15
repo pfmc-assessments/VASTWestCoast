b06f58cd3a2192591ad3dcd3f0737a8b2706601d
kellijohnson-NOAA <kelli.johnson@nooa.gov>
2020-10-15 03:28:41 -0700

    fix example to run with new settings

--- tests/testthat/test-minimalexample.R
+++ tests/testthat/test-minimalexample.R
@@ -6,13 +6,12 @@ wd.old <- getwd()
 temp_path <- file.path(tempdir(), "eg")
 dir.create(temp_path, showWarnings = FALSE)
 setwd(temp_path)
-version <- tail(gsub(".cpp", "", 
-    dir(system.file("executables", package = "VAST"))), 1)
 
 Sim_Settings <- list(
   "Species" = "WCGBTS_Anoplopoma_fimbria",
   "ObsModelcondition" = c(2, 0),
   "nknots" = 500,
+  "overdispersion" = NULL,
   "strata" = data.frame("STRATA" = c("All_areas", "North", "South"),
     north_border = c(49, 36, 49),
     south_border = c(32, 32, 36),
@@ -28,24 +27,23 @@ test_that("Testing database is saved in VAST_condition", {
   test <- VAST_condition(
     conditiondir = temp_path,
     data = Database,
-    settings = Sim_Settings, spp = Sim_Settings$Species,
-    overdispersion = NULL)
+    settings = Sim_Settings, spp = Sim_Settings$Species)
   expect_true(file.exists("Save.RData"))
   e1 <- new.env()
   base::load(file = "Save.RData", envir = e1)
   newnames <- ls(envir = e1)
   expect_true(all(newnames %in%
-    c("conditiondir", "Database", "info", "out", "maps", "overdispersion", "settings",
+    c("conditiondir", "Database", "info", "out", "maps", "settings",
       "spp", "survey")),
     info = "names in the file saved after conditioning changed")
   expect_equivalent(
     get("out", envir = e1)$parameter_estimates$AIC,
     62733.02,
     tolerance = 1e-5, info = "checking the AIC of the sablefish run")
+  dyn.unload(TMB::dynlib(gsub("\\.dll", "", getLoadedDLLs()[[
+    grep(get("out", envir = e1)$settings$Version,
+      names(getLoadedDLLs()))]][["path"]])))
 })
 
 setwd(wd.old)
-dyn.unload(getLoadedDLLs()[[
-  grep(version, names(getLoadedDLLs()))]][["path"]])
-
 unlink(temp_path, recursive = TRUE)
