4bfe21edca3e6855fa4e413318bfb0bda5b4f9f7
kellijohnson-NOAA <kelli.johnson@noaa.gov>
2020-10-20 05:33:29 -0700

    feat(get_error): load R objects and open source code for bad model
    to help people who don't have the directory cloned but they want
    to fix a bad model result

---
+++ R/get_error.R
@@ -0,0 +1,46 @@
+#' Get R Objects from Model with Error
+#'
+#' Load two \code{RData} objects, one with the error message
+#' and one with the R objects used to produce the model.
+#'
+#' @details
+#' This function will replace all objects in your current environment with
+#' those found using \code{load} if any names overlap. As recommended in
+#' \code{?load}, it is safer to use the \code{envir =} parameter to load
+#' into a different environment or use \code{attach} as a wrapper for
+#' \code{load()}. We leave this up to the user and use a default of loading
+#' everything into the current environment. We recommend running
+#' \code{ls()} prior to running this function so you can see
+#' which objects are new and which were previously in your environment.
+#'
+#' @template conditiondir
+#' @param envir The environment where the data should be loaded.
+#' The default, \code{parent.frame()} will load all objects to your current
+#' environment.
+#' @export
+#' @author Kelli Faye Johnson
+#' @return Several R objects are loaded into the specified \code{envir},
+#' including the model results, which is called 'out'. Additionally, a web
+#' browser is opened navigating users to the code of \code{\link{VAST_do}}
+#' to facilitate rapid rerunning of the model using the loaded objects.
+#'
+#' @examples
+#' # Create a new environment
+#' env1 <- new.env()
+#' # Load objects into this environment and see the error message from fit_model
+#' \dontrun{
+#' get_error(conditiondir = , envir = env1)
+#' ls(envir = env1)
+#' get("out", envir = env1)
+#' }
+#' rm(env1)
+get_error <- function(conditiondir, envir = parent.frame()) {
+  rdataobjfn <- dir(conditiondir, pattern = "Save\\.RData", full.names = TRUE)
+  load(rdataobjfn, envir = envir)
+  browseURL("https://raw.githubusercontent.com/nwfsc-assess/VASTWestCoast/master/R/VAST_spp.R")
+  message("Please follow the steps below:\n",
+    "(1) Manipulate the object named settings,\n",
+    "(2) Re-run 'VAST_mesh',\n",
+    "(3) Re-run 'FishStatsUtils::make_settings', and\n",
+    "(4) Re-run 'FishStatsUtils::fit_model'.")
+}
