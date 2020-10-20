7caae98cc18c6d34faf5fa4b9b83f45e250e9381
John Wallace <John.Wallace@noaa.gov>
2020-10-20 15:23:04 -0700

    Use Imap package when in Linux

--- R/plot_westcoast.R
+++ R/plot_westcoast.R
@@ -22,23 +22,27 @@
 #' @return A ggplot2 object providing a map of the US West Coast. The map is
 #' returned visibly to provide easy printing capabilities.
 #'
-plot_westcoast <- function(g,
-  xlim = c(-127.15, -116.50),
-  ylim = c(31.90, 49.50)) {
+plot_westcoast <- function (g, xlim = c(-127.15, -116.5), ylim = c(31.9, 49.5)) 
+{
 
-  world <- rnaturalearth::ne_countries(scale = "medium", returnclass = "sf")
-  if (missing(g)) {
-    g <- ggplot2::ggplot(data = world)
-  }
+   if(.Platform$OS.type == "windows") {
+   
+       world <- rnaturalearth::ne_countries(scale = "medium", 
+           returnclass = "sf")
+       if (missing(g)) {
+           g <- ggplot2::ggplot(data = world)
+       }
+       gg <- g + ggplot2::geom_sf(data = world) + ggplot2::coord_sf(xlim = xlim, 
+           ylim = ylim, expand = FALSE) + ggplot2::theme_bw() + 
+           ggplot2::labs(x = "", y = "")
+       return(gg)
+       
+    }  else {
+    
+      require(Imap) # https://github.com/John-R-Wallace-NOAA/Imap
+      Imap::imap(list(world.h.land, world.h.borders), col = 'black', poly = "grey90", longrange = xlim, latrange = ylim, bg = 'transparent', 
+          grid = TRUE, grid.col = 'grey92', xlab = "", ylab = "", axes = c(1,2), cex.axis = 0.7, aspect = 1.2, zoom = FALSE)
+   }
 
-  # Include the outline of the US West Coast and remove axes labels
-  gg <- g +
-    ggplot2::geom_sf(data = world) +
-    ggplot2::coord_sf(xlim = xlim,
-      ylim = ylim, expand = FALSE) +
-    ggplot2::theme_bw() +
-    ggplot2::labs(x = "", y = "")
-
-  # Return it visibly so users don't have use print()
-  return(gg)
 }
+
