26aa049d4357805cde5370bb0022a0bdc4b40ffa
John Wallace <John.Wallace@noaa.gov>
2020-10-21 15:50:36 -0700

    Silence warning
    st_crs<- : replacing crs does not reproject data; use st_transform for that

--- R/plot_westcoast.R
+++ R/plot_westcoast.R
@@ -29,7 +29,9 @@ plot_westcoast <- function (g, xlim = c(-127.15, -116.5), ylim = c(31.9, 49.5))
     }
     else {
         load(paste0(find.package("VASTWestCoast"), "/data/world_hires.RData"))
+        oldOpt <- options(warn = -1)
         sf::st_crs(world) = 4326
+        options(oldOpt)
     }
     if (missing(g)) {
         g <- ggplot2::ggplot(data = world)
